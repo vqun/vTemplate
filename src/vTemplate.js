f0d35c2c1d820e7a0a7aec34e9461d18b13c681a
vQun <zwq572@gmail.com>
2014-05-07 22:43:04 +0800

    fix bugs of each function and init if function

--- src/vTemplate.js
+++ src/vTemplate.js
@@ -1,14 +1,21 @@
 (function(Global) {
   var consts = {
-    "stplReg": /\<(\=)([^\<\=\>]+?)\>|\<(\@)([^\<\@\=]+?)\(\s*(?:\w+)(?:\s*\,\s*(?:\w+)){0,}\s*\)/gm,
-    "arrPush": Array.prototype.push
+    "stplReg": /\<(\=)([^\<\=\>]+?)\>|\<(\@)([^\<\@\=]+?)\(\s*(?:\w+(?:\.\w+)*)(?:\s*\,\s*(?:\w+(?:\.\w+)*)){0,}\s*\)/gm,
+    "arrPush": Array.prototype.push,
+    "keywords": ["alert", "break", "case", "catch", "continue", "debugger",
+      "default", "default", "delete", "do", "eval", "else", "finally",
+      "for", "function", "if", "in", "instanceof", "new", "return",
+      "switch", "this", "throw", "try", "typeof", "var", "void",
+      "while", "with", "Object", "prototype", "Array", "Number",
+      "String", "Boolean", "Null", "Undefined", "RegExp", "Function", "window",
+      "console", "arguments", "setTimeout", "setInterval", "clearTimeout", "clearInterval"]
   };
   var prefix = {
     "str": "out+=\'",
     "arrKw": "out+=",
     "attr": "out+=data."
   }
-  var posifix = {
+  var postfix = {
     "str": "\';",
     "semicolon": ";"
   }
@@ -35,36 +42,37 @@
     var compiled = "";
     var ignored = config && config.ignored || [];
     while(step = stplReg.exec(stpl)) {
-      compiled += prefix.str + stpl.slice(oldIndex, stplReg.lastIndex - step[0].length) + posifix.str;
+      compiled += prefix.str + stpl.slice(oldIndex, stplReg.lastIndex - step[0].length) + postfix.str;
       oldIndex = stplReg.lastIndex;
       if(step[1] == "\=") {
         // restore the function like arr.join("") because that.compile change the join("") to join(\"\")
-        var step_2 = step[2].replace(/\\\"/gm, "\"");
-        compiled += (inArray(ignored, step_2) ? prefix.arrKw : prefix.attr) + step_2 + posifix.semicolon;
+        step[2]=step[2].replace(/\\\"/gm, "\"");
+        var kw = step[2].split(".")[0];
+        compiled += (inArray(ignored, kw) ? prefix.arrKw : prefix.attr) + step[2] + postfix.semicolon;
       }else if(step[3] == "\@") {
         var subStpl = stpl.slice(oldIndex+1);
         var startFlag = "\<\@"+step[4]+"\(";
         var endFlag = "\<\/\@"+step[4]+"\>";
         // figure out the nex position argument and the sub-template between <@X> and </@X>
         var map = checkoutMap(startFlag, endFlag, subStpl);
-        if(!map) {
-          throw "Something wrong with your template"
-        }
+        if(!map) throw "Something wrong with your template";
         var mapStpl = map[1];
         var match = step[0];
+        // if(step[4]=="if")debugger
         // find out the helpers arguments
-        var args = match.slice(match.indexOf("(")+1, match.indexOf(")")).replace(/(\w+)/gm,"\"$1\"");
+        var args = match.slice(match.indexOf("(")+1, match.indexOf(")")).replace(/(\w+(\.\w+)*)/gm,"\"$1\"");
         // the helpers always have the a maped template and the ignored as the last two arguments
         eval("compiled+=helpers." + step[4] + "("+ args +",mapStpl,ignored)");
         // reset the lastIndex for the next search
         stplReg.lastIndex = oldIndex = oldIndex + map[0] + 1; // the next position
       }
     }
-    var tail = prefix.str+stpl.slice(oldIndex)+posifix.str;
+    var tail = prefix.str+stpl.slice(oldIndex)+postfix.str;
     return compiled + tail
   }
   var helpers = {
     "count": 0,
+    "prefix": "data.",
     "each": function(name, key, value, stpl, ignored) {
       var k = "k" + helpers.count;
       var curr = "curr" + helpers.count;
@@ -85,6 +93,21 @@
         )
         + compiled + "}";
       return ret
+    },
+    "if": function(condition, stpl, ignored) {
+      var _ignored = [];
+      consts.arrPush.apply(_ignored, ignored);
+      consts.arrPush.apply(_ignored, consts.keywords);
+      var word = /(\w+)(\.\w+)*/gm;
+      condition = condition.replace(word, function(m0, m1) {
+        console.log(m0)
+        if(!inArray(_ignored, m1)) return helpers.prefix + m0
+      });
+      var compiled = __compile__(stpl, {
+        "ignored": ignored
+      });
+      var ret = "if(" + condition +"){" + compiled + "}";
+      return ret
     }
   }
   function inArray(arr, i) {
@@ -102,13 +125,13 @@
     var sl = src.length, ml = map.length;
     var copyStr = str;
     while((index = copyStr.indexOf(map)) != -1) {
-      mapIndex += index+ml;
       step = copyStr.slice(0, index);
+      mapIndex += index+ml;
       if(step.indexOf(src) != -1) {
         copyStr = copyStr.slice(index+ml);
       }else {
         // [0] is the length of the mapped string and [1] is the mapped string
-        return [mapIndex, str.slice(0, mapIndex)]
+        return [mapIndex, str.slice(0, mapIndex - ml)]
       }
     }
     return false
